8bea0e28e547f8712753873df93a5d0cab0d0f19
JOEL-BINU-PHILIP <joelbinuphilip@gmail.com>
2026-01-05 20:34:24 +0530

    Added  home and about component

---
+++ src/app/modules/public/pages/services-browse/services-browse.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-services-browse',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './services-browse.component.html',
+  styleUrls: ['./services-browse.component.css']
+})
+export class ServicesBrowseComponent { }
