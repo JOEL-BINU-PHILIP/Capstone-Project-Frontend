36ec610f8567b9b40c010420bc13bdb0188c04b7
JOEL-BINU-PHILIP <joelbinuphilip@gmail.com>
2026-01-05 20:39:12 +0530

    Followed Best standards

---
+++ src/app/modules/service-manager/service-manager.routes.ts
@@ -0,0 +1,33 @@
+import { Routes } from '@angular/router';
+
+export const MANAGER_ROUTES: Routes = [
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./pages/dashboard/manager-dashboard.component').then(m => m.ManagerDashboardComponent)
+    },
+    {
+        path: 'technicians/pending',
+        loadComponent: () => import('./pages/pending-technicians/pending-technicians.component').then(m => m.PendingTechniciansComponent)
+    },
+    {
+        path: 'technicians',
+        loadComponent: () => import('./pages/all-technicians/all-technicians.component').then(m => m.AllTechniciansComponent)
+    },
+    {
+        path: 'bookings/pending',
+        loadComponent: () => import('./pages/pending-bookings/pending-bookings.component').then(m => m.PendingBookingsComponent)
+    },
+    {
+        path: 'bookings/:id',
+        loadComponent: () => import('../customer/pages/booking-detail/booking-detail.component').then(m => m.BookingDetailComponent)
+    },
+    {
+        path: 'bookings',
+        loadComponent: () => import('./pages/all-bookings/all-bookings.component').then(m => m.AllBookingsComponent)
+    },
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    }
+];
